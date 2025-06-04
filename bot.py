11f4747bef3f337b9a4f03b26bc5716ddce06413
Nika <avekek689@gmail.com>
2025-06-05 00:56:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,27 @@
-# generate_session.py
+# bot.py
+import asyncio
 from pyrogram import Client
 
-print("Создаем session-файл...")
-Client(
-    "my_account",  # Будет создан my_account.session
-    api_id=1234567,  # Ваш API_ID
-    api_hash="ваш_api_hash"  # Ваш API_HASH
-).start()
+app = Client(
+    "my_account",  # Используем созданный session-файл
+    api_id=1016382,
+    api_hash="c27834e5683d50a9bacf835a95ec4763",
+    workdir="."
+)
 
+async def main():
+    async with app:
+        while True:
+            try:
+                await app.send_message(
+                    -1002173353459,  # Ваш CHAT_ID
+                    "⏰ Автоматическое сообщение"
+                )
+                print("Сообщение отправлено!")
+                await asyncio.sleep(4 * 3600 + 60)  # 4ч 1мин
+            except Exception as e:
+                print(f"Ошибка: {e}")
+                await asyncio.sleep(60)
+
+if __name__ == "__main__":
+    asyncio.run(main())
