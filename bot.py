563b740b751adfbfbb98ad4ee678f1b046400f06
Nika <avekek689@gmail.com>
2025-06-04 23:44:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,20 +2,16 @@ import os
 import base64
 from pyrogram import Client
 
-# Данные из Secrets GitHub
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION_BASE64 = os.getenv("TG_SESSION_BASE64")
 
-# Жёстко прописываем CHAT_ID (ваш чат -1002173353459)
 CHAT_ID = int(os.getenv("CHAT_ID"))
 
-# Восстанавливаем сессию из base64
 if SESSION_BASE64:
     with open("my_account.session", "wb") as f:
         f.write(base64.b64decode(SESSION_BASE64))
 
-# Запускаем бота
 app = Client("my_account", API_ID, API_HASH)
 
 async def main():
